91affb62017dc6159843aa06b9c4d8c40cca2523
Mateo Zuluaga Loaiza <mateo.zuluaga@eia.edu.co>
2020-04-13 23:56:43 -0500

    login into moodle

--- eiacrawler/settings.py
+++ eiacrawler/settings.py
@@ -25,7 +25,7 @@ NEWSPIDER_MODULE = 'eiacrawler.spiders'
 #USER_AGENT = 'eiacrawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- eiacrawler/spiders/eiacast.py
+++ eiacrawler/spiders/eiacast.py
@@ -1,17 +1,47 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import os
 from scrapy.http import FormRequest
+from ..items import EiacrawlerItem
 
 
 class EiacastSpider(scrapy.Spider):
     name = 'eiacast'
-    allowed_domains = ['inicio.saber.eia.edu.co']
+    allowed_domains = ['eia.edu.co']
     start_urls = ['https://inicio.saber.eia.edu.co/login/index.php/']
 
     def parse(self, response):
         token = response.css(
-            '#page-wrapper #page #page-content #region-main-box #region-main div .card .card-block #costum-login').extract_first()
+            '#page-wrapper #page #page-content #region-main-box #region-main div .card .card-block div div form input::attr(value)').getall()[1]
         if token is not None:
             print(token)
-        else:
-            print('Error: login no encontrado')
+            self.logger.info("got response for %r" % response.url)
+            return [FormRequest.from_response(response, formid="login", formdata={
+                'anchor': '',
+                'logintoken': token,
+                'username': os.getenv("USERNAME"),
+                'password': os.getenv("PASSWORD"),
+
+            }, callback=self.go_to_videoteca)]
+
+    def go_to_videoteca(self, response):
+        # items = EiacrawlerItem()
+        self.logger.info("got response for %r" % response.url)
+        url = response.css(
+            '#page #page-content #region-bs-main-and-pre div #hexGrid .hex .hexIn .hexLink::attr(href)').getall()[5]
+        return response.follow(url, callback=self.go_to_library)
+
+    def go_to_library(self, response):
+        print('****************************************')
+        self.logger.info("got response for %r" % response.url)
+        value = response.css('form input::attr(value)').getall()
+        return [FormRequest.from_response(response, formid="login", formdata={
+                'username': value[0],
+                'password': value[1],
+                }, callback=self.parse_links)]
+
+    def parse_links(self, response):
+        print('****************************************')
+        self.logger.info("got response for %r" % response.url)
+        divs = response.css('div').getall()
+        print(divs)
